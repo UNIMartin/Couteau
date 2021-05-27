4987556f1aa21cde6d3984559f25e1d5754aeb1d
Ellafroekjaer <71000446+Ellafroekjaer@users.noreply.github.com>
2021-05-27 06:04:46 +0200

    Sidste rettelser

--- js/script.js
+++ js/script.js
@@ -1,20 +1,20 @@
-console.log('Jeg virker') //JS check
+console.log('Jeg virker'); //JS check
 
 // Toggle mobile menu
-const toggle = document.querySelector('.toggle')
-const menu = document.querySelector('.menu')
+const toggle = document.querySelector('.toggle');
+const menu = document.querySelector('.menu');
 
 function toggleMenu() {
   if (menu.classList.contains('active')) {
-    menu.classList.remove('active')
+    menu.classList.remove('active');
     toggle.querySelector('a').innerHTML = "<i class='fas fa-bars'></i>"
   } else {
-    menu.classList.add('active')
+    menu.classList.add('active');
     toggle.querySelector('a').innerHTML = "<i class='fas fa-times'></i>"
   }
 }
 
-toggle.addEventListener('click', toggleMenu, false)
+toggle.addEventListener('click', toggleMenu, false);
 
 // Submenu
 const items = document.querySelectorAll('.item')
@@ -25,7 +25,7 @@ function toggleItem() {
     this.classList.remove('submenu-active')
     arrow.classList.remove('rotate')
   } else if (menu.querySelector('.submenu-active')) {
-    menu.querySelector('.submenu-active').classList.remove('submenu-active')
+    menu.querySelector('.submenu-active').classList.remove('submenu-active');
     this.classList.add('submenu-active')
     // arrow.classList.add('.rotate')
   } else {
@@ -36,8 +36,8 @@ function toggleItem() {
 
 for (let item of items) {
   if (item.querySelector('.submenu')) {
-    item.addEventListener('click', toggleItem, false)
-    item.addEventListener('keypress', toggleItem, false)
+    item.addEventListener('click', toggleItem, false);
+    item.addEventListener('keypress', toggleItem, false);
   }
 }
 // Close submenu from anywhere in the page
@@ -45,8 +45,8 @@ function closeSubmenu(e) {
   let isClickInside = menu.contains(e.target)
 
   if (!isClickInside && menu.querySelector('.submenu-active')) {
-    menu.querySelector('.submenu-active').classList.remove('submenu-active')
-    arrow.classList.remove('rotate')
+    menu.querySelector('.submenu-active').classList.remove('submenu-active');
+    arrow.classList.remove('rotate');
   }
 }
 document.addEventListener('click', closeSubmenu, false)
@@ -54,27 +54,27 @@ document.addEventListener('click', closeSubmenu, false)
 // Google Map API
 function initMap() {
   // The location of Couteau
-  const uluru = { lat: 56.152538, lng: 10.204006 }
+  const couteau = { lat: 56.152538, lng: 10.204006 }
   // The map, centered at Couteau
   const map = new google.maps.Map(document.getElementById('map'), {
     zoom: 17,
-    center: uluru,
-  })
+    center: couteau,
+  });
   // The marker, positioned at Couteau
   const marker = new google.maps.Marker({
-    position: uluru,
+    position: couteau,
     map: map,
   })
 }
 
 //Login form (modal)
-document.getElementById('login_btn').addEventListener('click', show_modal)
+document.getElementById('login_btn').addEventListener('click', show_modal);
 
 function show_modal() {
   document.querySelector('.modal').style.display = 'block'
 }
 
-document.querySelector('.close').addEventListener('click', hide_modal)
+document.querySelector('.close').addEventListener('click', hide_modal);
 
 function hide_modal() {
   document.getElementById('id01').style.display = 'none'
